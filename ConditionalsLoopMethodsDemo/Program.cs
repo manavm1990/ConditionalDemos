038c369fb17e824e671082ce0ea7a01ad0dc22f8
Manav Misra <manavm@visionwebsoft.com>
2022-03-10 10:29:50 -0600

    Output a string showing results of the 'in range' method

--- ConditionalsLoopMethodsDemo/Program.cs
+++ ConditionalsLoopMethodsDemo/Program.cs
@@ -6,8 +6,23 @@ namespace ConditionalsLoopMethodsDemo
     {
        static void Main()
         {
-            // TODO: Implement method to check if a number is in range of another number
+            Console.Write("Enter an initial number");
+            int startingNum = int.Parse(Console.ReadLine());
 
+            Console.Write("Enter an target number");
+            int targetNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a range");
+            int range = int.Parse(Console.ReadLine());
+
+            // TODO: Write the result by calling IsWithinRange
+            Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. It's {IsWithinRange(startingNum, targetNum, range)}");
+        }
+
+        // access modifier - static (no not) - return type - name(parms in here)
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            return (num - target) <= range;
         }
     }
 }
