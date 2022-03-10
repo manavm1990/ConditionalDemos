e6fc97fa23e472528ca35cf71b2bda652274c525
Manav Misra <manavm@visionwebsoft.com>
2022-03-10 11:06:35 -0600

    Clarify solution with comments

--- ConditionalsLoopMethodsDemo/Program.cs
+++ ConditionalsLoopMethodsDemo/Program.cs
@@ -17,14 +17,14 @@ namespace ConditionalsLoopMethodsDemo
 
             string results = IsWithinRange(startingNum, targetNum, range) ? "It's within range." : "It's not within range.";
 
-            Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. " +
-                $"{results}");
+            Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. {results}");
         }
 
         // access modifier - static (no not) - return type - name(parms in here)
         private static bool IsWithinRange(int starting, int target, int range)
         {
-            return (starting - target) <= range && (starting - target) >= -range ;
+            // true or false?  if starting - target is greater than or equal the negative range AND starting - target is less than or equal to range
+            return (starting - target) >= -range && (starting - target) <= range;
         }
     }
 }
